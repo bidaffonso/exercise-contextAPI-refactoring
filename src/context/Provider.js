fd58e418ff7394a43e136250d05f8c82420f6231
bidaffonso <bidaffondo@gmail.com>
2021-06-21 20:27:14 -0400

    Finalizado Provider

--- src/context/Provider.js
+++ src/context/Provider.js
@@ -20,8 +20,37 @@ class Provider extends React.Component {
     this.changeSignal = this.changeSignal.bind(this);
   }
 
+  moveCar(car, side) {
+    this.setState({
+      cars: {
+        ...this.state.cars,
+        [car]: side,
+      }
+    })
+  }
+
+changeSignal(payload) {
+  this.setState({
+    signal: {
+      ...this.state.signal,
+      color: payload,
+    }
+  })
+}
+
   render() {
-    return(null);
+    const context = {
+      ...this.state,
+      moveCar: this.moveCar,
+      changeSignal: this.changeSignal,
+    }
+
+    const { children } = this.props;
+    return(
+      <MyContext.Provider value={context}>
+        {children}
+      </MyContext.Provider>
+    );
   }
 }
 
